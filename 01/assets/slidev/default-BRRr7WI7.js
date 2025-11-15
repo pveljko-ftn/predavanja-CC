63f531cd912d8fce033fdc298088cfe64f680b76
Veljko Petrović <pveljko.ftn@gmail.com>
2025-11-15 17:47:52 +0100

    01

---
+++ 01/assets/slidev/default-BRRr7WI7.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-CPnldld4.js";import{_ as r}from"../index-DcCCc-5Q.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
