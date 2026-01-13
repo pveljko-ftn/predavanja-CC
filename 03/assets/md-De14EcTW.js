8412b59db86c9a4df3f497cfec828bc040fb7502
Veljko Petrović <pveljko.ftn@gmail.com>
2026-01-13 12:01:34 +0100

    03

---
+++ 03/assets/md-De14EcTW.js
@@ -0,0 +1,20 @@
+import{b as t,o as p,w as c,g as n,ad as a,v as r,x as o,T as l}from"./modules/vue-B--U7SYg.js";import{I as u}from"./slidev/default-86i5BpUo.js";import{u as d,f}from"./slidev/context-vXhlvO6f.js";import"./index-DMYPucYz.js";import"./modules/shiki-0kOzWxKF.js";const C={__name:"03.md__slidev_95",setup(m){const{$clicksContext:e,$frontmatter:i}=d();return e.setup(),(b,s)=>(p(),t(u,r(o(l(f)(l(i),94))),{default:c(()=>[...s[0]||(s[0]=[n("h2",null,"Primer (dela) sertifikata",-1),n("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[n("code",{class:"language-text"},[n("span",{class:"line"},[n("span",null,"Certificate:")]),a(`
+`),n("span",{class:"line"},[n("span",null,"    Data:")]),a(`
+`),n("span",{class:"line"},[n("span",null,"        Version: 3 (0x2)")]),a(`
+`),n("span",{class:"line"},[n("span",null,"        Serial Number:")]),a(`
+`),n("span",{class:"line"},[n("span",null,"            10:e6:fc:62:b7:41:8a:d5:00:5e:45:b6")]),a(`
+`),n("span",{class:"line"},[n("span",null,"        Signature Algorithm: sha256WithRSAEncryption")]),a(`
+`),n("span",{class:"line"},[n("span",null,"        Issuer: C=BE, O=GlobalSign nv-sa, CN=GlobalSign Organization Validation CA - SHA256 - G2")]),a(`
+`),n("span",{class:"line"},[n("span",null,"        Validity")]),a(`
+`),n("span",{class:"line"},[n("span",null,"            Not Before: Nov 21 08:00:00 2016 GMT")]),a(`
+`),n("span",{class:"line"},[n("span",null,"            Not After : Nov 22 07:59:59 2017 GMT")]),a(`
+`),n("span",{class:"line"},[n("span",null,"        Subject: C=US, ST=California, L=San Francisco, O=Wikimedia Foundation, Inc., CN=*.wikipedia.org")]),a(`
+`),n("span",{class:"line"},[n("span",null,"        Subject Public Key Info:")]),a(`
+`),n("span",{class:"line"},[n("span",null,"            Public Key Algorithm: id-ecPublicKey")]),a(`
+`),n("span",{class:"line"},[n("span",null,"                Public-Key: (256 bit)")]),a(`
+`),n("span",{class:"line"},[n("span",null,"            pub: ")]),a(`
+`),n("span",{class:"line"},[n("span",null,"                    00:c9:22:69:31:8a:d6:6c:ea:da:c3:7f:2c:ac:a5:")]),a(`
+`),n("span",{class:"line"},[n("span",null,"                    af:c0:02:ea:81:cb:65:b9:fd:0c:6d:46:5b:c9:1e:")]),a(`
+`),n("span",{class:"line"},[n("span",null,"                    9d:3b:ef")]),a(`
+`),n("span",{class:"line"},[n("span",null,"                ASN1 OID: prime256v1")]),a(`
+`),n("span",{class:"line"},[n("span",null,"                NIST CURVE: P-256")])])],-1),n("p",null,"(Wikipedia serfifkat koji su okačili na relevatan članak u tekst formi)",-1)])]),_:1},16))}};export{C as default};
