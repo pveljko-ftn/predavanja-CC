97701829eb990a5e828960a22793a854f3ffa796
Veljko Petrović <pveljko@uns.ac.rs>
2026-02-21 15:49:02 +0100

    02

---
+++ 02/assets/md-D6Mm3XyD.js
@@ -0,0 +1,8 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-DHeqwYra.js";import{b as p,o as c,w as n,g as s,e as u,m,ad as a,v as d,x as f,T as t}from"./modules/vue-B--U7SYg.js";import{I as _}from"./slidev/default-CTHkqyk_.js";import{u as k,f as g}from"./slidev/context-CqRUKiaa.js";import"./modules/unplugin-icons-Dafapu7P.js";import"./index-6eePDshX.js";import"./modules/shiki-0kOzWxKF.js";const T={__name:"02.md__slidev_107",setup(h){const{$clicksContext:l,$frontmatter:i}=k();return l.setup(),(x,e)=>{const o=r;return c(),p(_,d(f(t(g)(t(i),106))),{default:n(()=>[e[1]||(e[1]=s("h2",null,"Rezultat",-1)),u(o,m({},{title:"",ranges:[]}),{default:n(()=>[...e[0]||(e[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,".")]),a(`
+`),s("span",{class:"line"},[s("span",null,"..")]),a(`
+`),s("span",{class:"line"},[s("span",null,"chroot_works")]),a(`
+`),s("span",{class:"line"},[s("span",null,"tst.c")]),a(`
+`),s("span",{class:"line"},[s("span",null,"tst")]),a(`
+`),s("span",{class:"line"},[s("span",null,"usr")]),a(`
+`),s("span",{class:"line"},[s("span",null,"lib")]),a(`
+`),s("span",{class:"line"},[s("span",null,"lib64")])])],-1)])]),_:1},16)]),_:1},16)}}};export{T as default};
